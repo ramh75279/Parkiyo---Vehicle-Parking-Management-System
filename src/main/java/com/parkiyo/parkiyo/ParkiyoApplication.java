51a6695486177fd8b38b6c83ba288eaa5b76335a
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-03-06 20:28:08 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/parkiyo/parkiyo/ParkiyoApplication.java
@@ -0,0 +1,13 @@
+package com.parkiyo.parkiyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkiyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkiyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkiyo/parkiyo/ParkiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkiyo.parkiyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkiyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
