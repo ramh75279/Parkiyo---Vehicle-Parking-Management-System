950bddfb99bcf0bc9a15b282876d8e5c00e2c8b6
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-03-06 23:31:05 +0530

    Connect Spring Boot application to MySQL database

--- src/main/java/com/parkiyo/parkiyo/ParkiyoApplication.java
+++ src/main/java/com/parkiyo/parkiyo/ParkiyoApplication.java
@@ -2,19 +2,11 @@ package com.parkiyo.parkiyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class ParkiyoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ParkiyoApplication.class, args);
     }
-
 }
