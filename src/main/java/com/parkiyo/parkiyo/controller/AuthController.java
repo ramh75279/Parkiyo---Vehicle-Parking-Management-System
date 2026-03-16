dd8dc245888b8d30a105ec48f83558e3063563d7
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-03-16 20:48:29 +0530

    Connecting home page and login page for key understanding of controllers and jsp files

--- src/main/java/com/parkiyo/parkiyo/controller/AuthController.java
+++ src/main/java/com/parkiyo/parkiyo/controller/AuthController.java
@@ -6,34 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    // Login page
     @GetMapping("/login")
-    public String loginPage() {
-        return "login";
+    public String login() {
+        return "auth/login";
     }
 
-    // Register page
     @GetMapping("/register")
-    public String registerPage() {
-        return "register";
-    }
-
-    // Forgot password page
-    @GetMapping("/forgotpassword")
-    public String forgotPasswordPage() {
-        return "forgotpassword";
-    }
-
-    // Logout page
-    @GetMapping("/logout")
-    public String logoutPage() {
-        return "logout";
-    }
-
-    // Access denied page
-    @GetMapping("/accessdenied")
-    public String accessDeniedPage() {
-        return "accessdenied";
+    public String register() {
+        return "auth/register";
     }
 
 }
