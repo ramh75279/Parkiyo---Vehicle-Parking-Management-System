61f9363841a71735b90b716a0649592dd3134abd
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-03-22 23:34:05 +0530

    Converting individual files to be able to work with Thymleaf

--- src/main/java/com/parkiyo/parkiyo/controller/PageController.java
+++ src/main/java/com/parkiyo/parkiyo/controller/PageController.java
@@ -2,23 +2,26 @@ package com.parkiyo.parkiyo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
+    public String root() {
+        return "common/home";
+    }
+
+    @GetMapping("/common/home")
     public String home() {
-        return "home";
+        return "common/home";
     }
 
-    @GetMapping("/features")
-    public String features() {
-        return "features";
+    @GetMapping("/{folder}/{page}")
+    public String loadPage(@PathVariable String folder,
+                           @PathVariable String page) {
+        return folder + "/" + page;
     }
 
-    @GetMapping("/solutions")
-    public String solutions() {
-        return "solutions";
-    }
 
 }
