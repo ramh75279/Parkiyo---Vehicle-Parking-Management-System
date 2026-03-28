dc3e78dede9b76eb794974ada24527c5fb9e9ad1
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-03-28 17:29:50 +0530

    Initializing models to create objects.

---
+++ src/main/java/com/parkiyo/parkiyo/model/User.java
@@ -0,0 +1,74 @@
+package com.parkiyo.parkiyo.model;
+
+import com.parkiyo.enums.Role;
+import com.parkiyo.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    private String passwordResetToken;
+    private LocalDateTime passwordResetTokenExpiry;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean emailNotificationsEnabled = true;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean smsNotificationsEnabled = true;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Wallet wallet;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Vehicle> vehicles;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Reservation> reservations;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Notification> notifications;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
