1e2eb9ff2402eb42414680dd739cc3b0319fe62d
IT25101695 <IT25101695@my.sliit.lk>
2026-04-21 21:46:48 +0530

    Delete ParkingSessionRepository.java

--- src/main/java/com/parkiyo/parkiyo/repository/ParkingSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parkiyo.parkiyo.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingSessionRepository extends ParkingRecordRepository {
-}
