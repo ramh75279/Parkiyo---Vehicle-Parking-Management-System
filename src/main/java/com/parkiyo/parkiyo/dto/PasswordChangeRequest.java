78446dbe1c6fcfef368d2fbfa68c1c5fc041b5bb
IT25101695 <IT25101695@my.sliit.lk>
2026-04-21 20:00:48 +0530

    Adding passwordchangerequest to dto

---
+++ src/main/java/com/parkiyo/parkiyo/dto/PasswordChangeRequest.java
@@ -0,0 +1,11 @@
+package com.parkiyo.parkiyo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PasswordChangeRequest {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+}
