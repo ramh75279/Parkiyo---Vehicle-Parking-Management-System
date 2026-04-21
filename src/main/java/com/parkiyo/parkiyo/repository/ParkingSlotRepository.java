7c957a297ef8c9a0b0be927c001b5dcfc1131641
IT25101686 - Ashwini.J <IT25101686@my.sliit.lk>
2026-04-21 15:18:40 +0530

    Update ParkingSlotRepository.java

--- src/main/java/com/parkiyo/parkiyo/repository/ParkingSlotRepository.java
+++ src/main/java/com/parkiyo/parkiyo/repository/ParkingSlotRepository.java
@@ -1,4 +1,27 @@
 package com.parkiyo.parkiyo.repository;
 
-public class ParkingSlotRepository {
+import com.parkiyo.parkiyo.model.ParkingSlot;
+import com.parkiyo.parkiyo.enums.SlotStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ParkingSlotRepository extends JpaRepository<ParkingSlot, Long> {
+
+    Optional<ParkingSlot> findBySlotNumber(String slotNumber);
+
+    boolean existsBySlotNumber(String slotNumber);
+
+    List<ParkingSlot> findByStatus(SlotStatus status);
+
+    List<ParkingSlot> findByZone(String zone);
+
+    List<ParkingSlot> findByZoneAndStatus(String zone, SlotStatus status);
+
+    List<String> findDistinctZoneBy();
+
+    long countByStatus(SlotStatus status);
 }
