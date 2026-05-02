36748686b225c09f41542d97c9710fcdcae59b33
Faleel M. H. <it25101663@my.sliit.lk>
2026-05-02 21:21:09 +0530

    Update ParkiyoApplication.java

--- src/main/java/com/parkiyo/parkiyo/ParkiyoApplication.java
+++ src/main/java/com/parkiyo/parkiyo/ParkiyoApplication.java
@@ -2,9 +2,13 @@ package com.parkiyo.parkiyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EntityScan("com.parkiyo.parkiyo.model")
+@EnableJpaRepositories("com.parkiyo.parkiyo.repository")
 @EnableAsync
 public class ParkiyoApplication {
 
