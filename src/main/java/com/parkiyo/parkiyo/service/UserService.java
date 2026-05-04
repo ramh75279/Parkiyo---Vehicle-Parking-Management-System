fd82c7dc2675bf94f09ce4145804d8fabf4d5ac7
Premashainath Kirubakaran <it25101814@my.sliit.lk>
2026-05-04 08:58:27 +0530

    Update UserService.java

--- src/main/java/com/parkiyo/parkiyo/service/UserService.java
+++ src/main/java/com/parkiyo/parkiyo/service/UserService.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -34,6 +35,11 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
 
 @Service
 @RequiredArgsConstructor
