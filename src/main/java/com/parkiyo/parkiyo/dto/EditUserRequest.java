fdf203f1479fcf7b301542b89d5f40ffbf346113
IT25101695 <IT25101695@my.sliit.lk>
2026-05-05 11:59:10 +0530

    Update EditUserRequest.java

--- src/main/java/com/parkiyo/parkiyo/dto/EditUserRequest.java
+++ src/main/java/com/parkiyo/parkiyo/dto/EditUserRequest.java
@@ -1,14 +1,25 @@
 package com.parkiyo.parkiyo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class EditUserRequest {
 
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
     private String phone;
+
     private String role;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
     private String status;
 }
